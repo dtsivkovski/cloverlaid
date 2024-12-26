1c039705cd32a2c822eb7cf59d825455ce5aee29
dtsivkovski <dtsivkovs@gmail.com>
2024-12-26 00:11:03 -0700

    cloverlaid icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.ico" />
+      </head>
       <body
         className={`nunito-sans-standard antialiased`}
       >
