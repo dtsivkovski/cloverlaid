11c0305529ac411c917ca71f332f42965f058ee7
dtsivkovski <dtsivkovs@gmail.com>
2024-12-25 23:49:40 -0700

    visual additions to page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,18 +5,27 @@ import Link from "next/link";
 const MainHeader = () => {
     return ( 
         <div className="flex flex-row items-center justify-between w-full p-4">
-        <h1 className="text-3xl font-bold text-center">Easy Overlay</h1>
-        <Link href="https://github.com/dtsivkovski">
-          <Button variant="outline" size="icon"> 
-            <Image
-              src="/github-mark.svg"
-              alt="Github"
-              width={24}
-              height={24}
-            />
-          </Button>
-        </Link>
-      </div>
+            <div className="flex flex-row items-center justify-start gap-x-2">
+                <Image 
+                    src="/Cloverlaid.svg" 
+                    alt="Cloverlaid" 
+                    width={24} 
+                    height={24} 
+                    draggable={false}
+                />
+                <h1 className="text-3xl font-bold text-center select-none rgb-text-clip maven-pro-standard">Cloverlaid</h1>
+            </div>
+            <Link href="https://github.com/dtsivkovski">
+            <Button variant="outline" size="icon" className="mx-2"> 
+                <Image
+                src="/github-mark.svg"
+                alt="Github"
+                width={24}
+                height={24}
+                />
+            </Button>
+            </Link>
+        </div>
     );
 }
  
